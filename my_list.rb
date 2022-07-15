8c429e15c2d2a8687db902aa5cdc2fd1c548d5eb
Theophileaseh <theophilenwachan@gmail.com>
2022-07-15 13:31:04 +0100

    add each method, test output

--- my_list.rb
+++ my_list.rb
@@ -6,5 +6,20 @@ class MyList
   def initialize(*list)
     @list = list
   end
+
+  def each(&block)
+    @list.each(&block)
+  end
 end
 
+list = MyList.new(1, 2, 3, 4)
+
+list.all? { |e| e < 5 }
+
+list.all? { |e| e > 5 }
+
+list.any? { |e| e == 2 }
+
+list.any? { |e| e == 5 }
+
+list.filter(&:even?)
